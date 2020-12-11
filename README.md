42d6795b062de4d641fe378da6f7c7efac3fc7eb
Dan <dan.z.haas@gmail.com>
2020-12-11 00:43:33 -0500

    connecting to mongoDB with Mongoose, route files with Express Router

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dev-connector
+
+Project begun 12/10/2020
+https://www.udemy.com/course/mern-stack-front-to-back/
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require('express'); // brings in the package
+const connectDB = require('./config/db');   //brings in database connection code
 
-const app = express();
+const app = express();  //initialize app variable
 
-app.get('/', (req, res) => res.send('API Running'));
+connectDB();    // Connect Database
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('API Running')); // When the server receives a GET request from a url ending with /, send a response with data content 'API Running'
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.use('/api/users', require('./routes/api/users')); //define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;  //The port is looking for an environmental variable.  On this project it will come from Heroku, the SaaS where the server is deployed.  If no environmental varibale is present (local production), it uses local port 5000 instead.
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));  // server app should listen for a port; upon connection, logs the port referenced.
