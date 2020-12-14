7d492b4f6fd4950923f2beb2edb9a9da104ae8b3
danzhaas <dan.z.haas@gmail.com>
2020-12-14 01:22:03 -0500

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,17 @@
 const express = require ('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require ('gravatar');  // user avatar database
+const bcrypt = require('bcryptjs'); // encryption package
+const { check, validationResult } = require('express-validator'); //the methods for request validation
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register route
 // @access  Public
 router.post(
-    '/', 
-    [
+    '/',    // route
+    [   // second argument is an array in which fields are checked against validators
         check ('name', 'Name is required')
             .not()
             .isEmpty(),
@@ -16,12 +20,46 @@ router.post(
         check ('password', 'Please enter a password with 6 or more characters')
             .isLength({ min: 6 }),
     ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })  // code 400: bad request
+    async (req, res) => {   //async because this will request promises from the database
+        const errors = validationResult(req);   // validationResult contains the array of error messages generated from failed checks above
+        if(!errors.isEmpty()) { //if the errors array is not empty
+            return res.status(400).json({ errors: errors.array() })  // code 400: bad request, in this case validation failed
         }
-        res.send('User route');
+            
+        const { name, email, password } = req.body; //deconstruct user fields from the request object
+
+        try {
+            
+            let user = await User.findOne({ email }); // see if a user with this email exists; 
+            if (user) {  // if user exists, respond with error; 
+                return res.status(400).json({ errors: [ { msg: 'User already exists' }] });
+            }
+            
+            const avatar = gravatar.url(email, {  // get user's gravatar registered to their email; 
+                s: '200',   //size in px
+                r: 'pg',    //rating.  pg "may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence."
+                d: 'mm'     // default: mm stands for a default "mystery man"
+            })
+
+            user = new User({   //create a new User document in the DB containing the posted
+                name,
+                email,
+                avatar,
+                password    // note: unencrypted.  This needs to be fixed right away.
+            })
+            
+            const salt = await bcrypt.genSalt(10); // Encrypt password part 1: generate salt with 10 digits; 
+            user.password = await bcrypt.hash(password, salt);  // Encrypt password part 2: generate the hashed password;
+
+            await user.save();  //actually save the user document to the DB
+            // return jsonwebtoken
+            res.send('User registered');
+
+        } catch(err) {
+            
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        };
     }
 );
 
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();  //initialize app variable
 
 connectDB();    // Connect Database
 
-app.use(express.json({ extended:false }));    //Init Middleware
+//Middleware
+app.use(express.json({ extended:false }));    // parse json objects in requests - this is the middleware formerly known as body-parser
 
+//Routing
 app.get('/', (req, res) => res.send('API Running')); // When the server receives a GET request from a url ending with /, send a response with data content 'API Running'
 
 app.use('/api/users', require('./routes/api/users')); //define routes
@@ -17,3 +19,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;  //The port is looking for an environmental variable.  On this project it will come from Heroku, the SaaS where the server is deployed.  If no environmental varibale is present (local production), it uses local port 5000 instead.
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));  // server app should listen for a port; upon connection, logs the port referenced.
+
