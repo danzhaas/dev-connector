5ed05f717a9401628da41831134fd467b1f7498d
danzhaas <dan.z.haas@gmail.com>
2020-12-17 00:00:24 -0500

    user authentication/login route; creating profile model; get current user profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require ('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');  // brings in jwt auth middleware
+const bcrypt = require('bcryptjs'); // encryption package
+const jwt = require('jsonwebtoken');    // js web token
+const config = require('config');   //allows us to access other json files 
+const { check, validationResult } = require('express-validator'); //the methods for request validation
 
 const User = require('../../models/User');  //brings in User model
 
@@ -17,5 +21,63 @@ router.get('/', auth, async (req, res) => { // note auth middleware and async be
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+    '/',    // route
+    [   // second argument is an array in which fields are checked against validators
+        check ('email', 'Please include a valid email')
+            .isEmail(),
+        check ('password', 'Please enter a password with 6 or more characters')
+            .exists()
+    ],
+    async (req, res) => {   //async because this will request promises from the database
+        const errors = validationResult(req);   // validationResult contains the array of error messages generated from failed checks above
+        if(!errors.isEmpty()) { //if the errors array is not empty
+            return res.status(400).json({ errors: errors.array() })  // code 400: bad request, in this case validation failed
+        }
+            
+        const { email, password } = req.body; //deconstruct user fields from the request object
+
+        try {
+            
+            let user = await User.findOne({ email }); // see if a user with this email exists; 
+            if (!user) {  // if user exists, respond with error; 
+                return res
+                    .status(400)
+                    .json({ errors: [ { msg: 'Invalid credentials' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                .status(400)
+                .json({ errors: [ { msg: 'Invalid credentials' }] });
+            }
+
+            const payload = {   // formats user id for jwt generation
+                user: { 
+                    id: user.id
+                }
+            }
+            jwt.sign(   // returns token or error asynchronously
+                payload, 
+                config.get('jwtSecret'),    // this secret is in default.json
+                { expiresIn:360000 },   //how long in SECONDS the token will be valid
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });    // sends token to user
+                }
+            );
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        };
+    }
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require ('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me  
+// @desc    Get current user's profile (as opposed to all profiles)
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile
+            .findOne({ user: req.user.id })   // find profile where user value matches the user id decoded from the user's jwt.  
+            .populate('user', ['name', 'avatar']);  //populate [something] with name and avatar from user 
+
+        if (!profile) { // If there is no matching profile, 
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);  // Send response containing profile object.  
+    }
+    catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router;
