4001fbf7c2b2836ce7a8233ada56f91529eca84e
danzhaas <dan.z.haas@gmail.com>
2021-01-16 22:40:56 -0500

    Add, list, and delete education, experiences, and profile.  Profile display: finish profile actions & reducer, display profiles, addressing the console warnings, starting on the profile

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import DashboardActions from './DashboardActions'
-import { getCurrentProfile } from '../../actions/profile'
+import Experience from './Experience'
+import Education from './Education'
+import { deleteAccount, getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading }}) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading }, deleteAccount }) => {
     useEffect(() => {
         getCurrentProfile();
     }, [getCurrentProfile]);
@@ -22,6 +24,14 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         { profile !== null ? 
             <Fragment>
                 <DashboardActions />
+                <Experience experience={profile.experience} />
+                <Education education={profile.education} />
+
+                <div className="my-2">
+                    <button className="btn btn-danger" onClick={() => deleteAccount(user._id)}>
+                        <i className='fa fa-user-minus'></i> Delete My Account
+                    </button>
+                </div>
             </Fragment> 
             : 
             <Fragment>
@@ -37,7 +47,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired
+    profile: PropTypes.object.isRequired,
+    deleteAccount: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -45,4 +56,4 @@ const mapStateToProps = state => ({
     profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard)
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,7 +1,10 @@
 import { 
     PROFILE_ERROR,
     GET_PROFILE,
-    CLEAR_PROFILE
+    GET_PROFILES,
+    CLEAR_PROFILE,
+    UPDATE_PROFILE,
+    GET_REPOS
 } from '../actions/types';
 
 const initialState = {  
@@ -17,9 +20,16 @@ export default function ( state = initialState, action ) {
 
     switch (type) {
         case GET_PROFILE: 
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile:payload,
+                loading:false
+            };
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
                 loading:false
             }
         case PROFILE_ERROR: 
@@ -27,12 +37,18 @@ export default function ( state = initialState, action ) {
                 ...state,
                 error:payload,
                 loading: false
-            }
+            };
         case CLEAR_PROFILE:
             return {
                 ...state,
                 profile:null,
                 repos:[],
+                loading:false
+            };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
                 loading:false
             }
         default:
