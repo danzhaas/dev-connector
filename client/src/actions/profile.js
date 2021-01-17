39332fcf9f75d3d71fca2cea221d4915378b7dad
danzhaas <dan.z.haas@gmail.com>
2021-01-17 01:26:26 -0500

    Profile component including top, about, experience, education, and github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,12 +48,12 @@ export const getProfiles = () => async dispatch => {
 };
 
 // Get profile by ID
-export const profileById = userId => async dispatch => {
+export const getProfileById = userId => async dispatch => {
     try {
         const res = await axios.get(`/api/profile/user/${userId}`);
 
         dispatch({
-            type:GET_PROFILES,
+            type:GET_PROFILE,
             payload: res.data
         })
     } catch (err) {
