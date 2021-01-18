62a0fad22d4120ef52868c893fcd99ed99acd84c
danzhaas <dan.z.haas@gmail.com>
2021-01-18 02:41:42 -0500

    Posts and Comments: post reducer, action, and initial component; post item component; like & unlike functionality; deleting posts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // brings in the package
 const connectDB = require('./config/db');   //brings in database connection code
+// const path = require('path');
 
 const app = express();  //initialize app variable
 
@@ -15,6 +16,13 @@ app.use('/api/users', require('./routes/api/users')); //define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// if(process.env.NODE_ENV === 'production') {    // Serve static assets in production
+//     app.use(express.static('client/build')); // Set static folder
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 const PORT = process.env.PORT || 5000;  //The port is looking for an environmental variable.  On this project it will come from Heroku, the SaaS where the server is deployed.  If no environmental varibale is present (local production), it uses local port 5000 instead.
 
