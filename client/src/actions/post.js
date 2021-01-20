1ba85ffd9ab0ca2d475033d479994f1f9589a5b0
danzhaas <dan.z.haas@gmail.com>
2021-01-19 22:15:57 -0500

    Adding Posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
     GET_POSTS,
     POST_ERROR,
     UPDATE_LIKES,
-    DELETE_POST
+    DELETE_POST,
+    ADD_POST
 } from './types';
 
 // Get posts
@@ -61,14 +62,39 @@ export const removeLike = id => async dispatch => {
 // Delete Post
 export const deletePost = id => async dispatch => {
     try {
-        const res = await axios.delete(`/api/posts/${id}`);
+        await axios.delete(`/api/posts/${id}`);
 
         dispatch({
             type:DELETE_POST,
             payload: id
         });
 
-        dispatch(setAlert('Post Removed', 'Success'))
+        dispatch(setAlert('Post Removed', 'success'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// Add Post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post(`/api/posts`, formData, config);
+
+        dispatch({
+            type:ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'))
     } catch (err) {
         dispatch({
             type: POST_ERROR,
